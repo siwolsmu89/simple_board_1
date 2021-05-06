5228817d85938e2f19c63b98219d9c12a1b891dd
siwolsmu89 <siwol_smuire89@naver.com>
2021-05-06 15:11:51 +0900

    Add sections to ArticleDetail component 2021/05/06

--- simple_board_react/src/components/view/article-detail/ArticleDetail.js
+++ simple_board_react/src/components/view/article-detail/ArticleDetail.js
@@ -1,27 +1,51 @@
 import {Component} from "react";
+import {Link} from "react-router-dom";
+import homeIcon from "../../../resources/home-button.png";
+import './ArticleDetail.css';
 
 export default class ArticleDetail extends Component {
 
     render() {
-
         const { articles } = this.props;
         const { no } = this.props.match.params;
+        let articleBody;
 
         if (Number(no)) {
             const detailArticle = articles.find( article => article.no === Number(no));
-            return(
-                <div>
-                    <h1>{detailArticle.title}</h1>
-                    <p>{detailArticle.text}</p>
-                </div>
-            );
-        } else {
-            return (
-                <div>
-                    Article Not Found
+            articleBody = (
+                <div className="article-detail">
+                    <div className="article-detail-title">
+                        { detailArticle.title }
+                    </div>
+                    <div className="article-detail-content">
+                        { detailArticle.text }
+                    </div>
                 </div>
             );
         }
+
+        return (
+            <div className="simple-board-detail">
+                <section className="icon-box-wrapper">
+                    <div className="icon-box">
+                        <Link to="/">
+                            <img
+                                src={ homeIcon }
+                                alt="home button icon"
+                            />
+                        </Link>
+                    </div>
+                </section>
+                <section className="article-detail-wrapper">
+                    { articleBody }
+                </section>
+                <section className="article-comment-wrapper">
+                    <div className="article-detail-comment">
+                        Comments here
+                    </div>
+                </section>
+            </div>
+        );
 
     }
 }
